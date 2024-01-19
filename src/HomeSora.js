353ec39d9eb41053072c28e91a11798cf0ad6850
sorasin91 <sora.sin91@gmail.com>
2024-01-19 23:05:17 +0800

    updated Home js and css file

--- src/HomeSora.js
+++ src/HomeSora.js
@@ -5,7 +5,6 @@ import './homeSora.css';
 function HomeSora() {
   return (
     <div>
-      <>
         <div className="banner" style={{ backgroundImage: `url(${image})`}}>
             <div className="intro">
               <h1>Mobile Gadget</h1>
@@ -32,14 +31,14 @@ function HomeSora() {
             <div className="row">
               {/* card 1 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
+                    id="home-card-img"
                     src="./assets/top-product-iphone-9.jpg"
                     alt="Card image cap"
-                    height="250px"
                   />
-                  <div className="card-body">
+                  <div className="card-body" id="home-card-body">
                     <h4 className="card-title">iPhone 9</h4>
                     <p className="card-text">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -58,12 +57,12 @@ function HomeSora() {
               </div>
               {/* card 2 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
+                    id="home-card-img"
                     src="./assets/top-product-huawei-p30.jpg"
                     alt="Card image cap"
-                    height="250px"
                   />
                   <div className="card-body">
                     <h4 className="card-title">Huawei P30</h4>
@@ -84,12 +83,12 @@ function HomeSora() {
               </div>
               {/* card 3 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
+                    id="home-card-img"
                     src="./assets/top-product-iphone-x.jpg"
                     alt="Card image cap"
-                    height="250px"
                   />
                   <div className="card-body">
                     <h4 className="card-title">iPhone X</h4>
@@ -118,12 +117,12 @@ function HomeSora() {
             <div className="row">
               {/* service 1 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
                     src="./assets/services-we-offer-1.jpg"
                     alt="Card image cap"
-                    height="250px"
+                    id="home-card-img"
                   />
                   <div className="card-body">
                     <h4 className="card-title">Lorem, ipsum.</h4>
@@ -144,12 +143,12 @@ function HomeSora() {
               </div>
               {/* service 2 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
                     src="./assets/services-we-offer-2.jpg"
                     alt="Card image cap"
-                    height="250px"
+                    id="home-card-img"
                   />
                   <div className="card-body">
                     <h4 className="card-title">Lorem, ipsum.</h4>
@@ -170,12 +169,12 @@ function HomeSora() {
               </div>
               {/* service 3 */}
               <div className="col-md-4 py-3 py-md-0">
-                <div className="card">
+                <div className="card" id="home-card">
                   <img
                     className="card-img-top"
                     src="./assets/services-we-offer-3.jpg"
                     alt="Card image cap"
-                    height="250px"
+                    id="home-card-img"
                   />
                   <div className="card-body">
                     <h4 className="card-title">Lorem, ipsum.</h4>
@@ -197,7 +196,6 @@ function HomeSora() {
             </div>
           </div>
         </section>
-      </>
     </div>
   );
 }
